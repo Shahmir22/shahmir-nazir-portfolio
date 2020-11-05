1e1683dc8a552d061ee7f6d8f7688484eca0ef27
ShahmirFarooqui <shahmirfarooqui91@gmail.com>
2020-11-04 22:29:02 -0500

    created new folders and files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+RReactDOM.render(
+    <Router>
+        <App />
+    </Router>
+    , document.getElementById('root')
+);
